4bf948d6fb5e95b320ffe0b5b82484a19925a048
PabluxFirpux <pfirpomolina@gmail.com>
2024-01-04 19:56:39 +0100

    FrequencyIndexer

---
+++ src/main/app.java
@@ -0,0 +1,9 @@
+package main;
+
+import model.FrequencyIndexer;
+
+public class app {
+    public static void main(String[] args) {
+        System.out.println(FrequencyIndexer.index("Hello, World!"));
+    }
+}
---
+++ src/model/FrequencyIndexer.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class FrequencyIndexer {
+    public static Dictionary<Character, Integer> index(String text) {
+        Dictionary<Character, Integer> frequencyIndex = new Hashtable<>();
+        for(Character c: text.toCharArray()) {
+            if(frequencyIndex.get(c) == null) {
+                frequencyIndex.put(c, 1);
+            } else {
+                frequencyIndex.put(c, frequencyIndex.get(c) + 1);
+            }
+        }
+        return frequencyIndex;
+    }
+}
